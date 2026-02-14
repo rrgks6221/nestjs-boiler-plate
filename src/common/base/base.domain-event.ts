51f4d0623b5c2a81235c627df9bbe0c62972aba8
LeeSeokHo <rrgks@naver.com>
2026-02-14 15:37:42 +0900

    feat: base doamin event 추가

---
+++ src/common/base/base.domain-event.ts
@@ -0,0 +1,21 @@
+import { TSID } from 'tsid-ts';
+
+type Aggregate = 'User';
+
+export abstract class DomainEvent<Payload = Record<string, any>> {
+  id: string;
+  aggregateId: string;
+  abstract readonly aggregate: Aggregate;
+  eventName: string;
+  eventPayload: Payload;
+  occurredAt: Date;
+  version: number;
+
+  constructor(aggregateId: string, eventPayload: Payload) {
+    this.id = TSID.create().number.toString();
+    this.aggregateId = aggregateId;
+    this.eventName = this.constructor.name;
+    this.eventPayload = eventPayload;
+    this.occurredAt = new Date();
+  }
+}
